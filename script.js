d1799db9c9e0f7f3b8eb2f69433803e5ad737ca2
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-04-05 11:48:52 -0700

    corrected time display

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-var now = dayjs().format('MMM D, YYYY');
-$('#currentDay').text(now);
-console.log(now)
+
+// console.log(now)
+
 
 function updateClock () {
-  var currentTime = new Date ();
-  var currentHour = currentTime.getHours ();
-  var currentMinute = currentTime.getMinutes ();
-  var currentSeconds = currentTime.getSeconds ();
-
-  currentMinute = (currentMinute < 10 ? "0" : "") + currentMinute;
-  currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
-
-  var timeOfDay = ( currentHour < 12 ) ? "AM" : "PM";
-  currentHour = (currentHour > 12) ? currentHour - 12 : currentHour;
-  currentHour = (currentHour ==  0) ? 12 : currentHour;
-  var currentTimeString = currentHour + ":" + currentMinute + ":" + currentSeconds + " " + timeOfDay;
-  $("#currentTime").html(currentTimeString);
+    var now = dayjs().format('MMM D, YYYY h:mm:ss A');
+    $('#currentDay').text(now);
 }
+
+// function updateClock () {
+//   var currentTime = new Date ();
+//   var currentHour = currentTime.getHours ();
+//   var currentMinute = currentTime.getMinutes ();
+//   var currentSeconds = currentTime.getSeconds ();
+
+//   currentMinute = (currentMinute < 10 ? "0" : "") + currentMinute;
+//   currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+
+//   var timeOfDay = ( currentHour < 12 ) ? "AM" : "PM";
+//   currentHour = (currentHour > 12) ? currentHour - 12 : currentHour;
+//   currentHour = (currentHour ==  0) ? 12 : currentHour;
+//   var currentTimeString = currentHour + ":" + currentMinute + ":" + currentSeconds + " " + timeOfDay;
+//   $("#currentTime").html(currentTimeString);
+// }
 $(document).ready(function() {
-  setInterval('updateClock()', 1000);
+  setInterval(updateClock, 1000);
 });
