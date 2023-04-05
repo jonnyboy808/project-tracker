b997b6ba8791583f3a18eef1c46bcbab09e1de36
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-04-05 11:38:34 -0700

    added styling time and date to header

---
+++ script.js
@@ -0,0 +1,22 @@
+var now = dayjs().format('MMM D, YYYY');
+$('#currentDay').text(now);
+console.log(now)
+
+function updateClock () {
+  var currentTime = new Date ();
+  var currentHour = currentTime.getHours ();
+  var currentMinute = currentTime.getMinutes ();
+  var currentSeconds = currentTime.getSeconds ();
+
+  currentMinute = (currentMinute < 10 ? "0" : "") + currentMinute;
+  currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+
+  var timeOfDay = ( currentHour < 12 ) ? "AM" : "PM";
+  currentHour = (currentHour > 12) ? currentHour - 12 : currentHour;
+  currentHour = (currentHour ==  0) ? 12 : currentHour;
+  var currentTimeString = currentHour + ":" + currentMinute + ":" + currentSeconds + " " + timeOfDay;
+  $("#currentTime").html(currentTimeString);
+}
+$(document).ready(function() {
+  setInterval('updateClock()', 1000);
+});
